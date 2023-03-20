0b236c82b4fe115ffd879c24917ceedd26f02ae3
Harne Nim <harne_@naver.com>
2023-03-20 19:18:56 +0900

    팝업 라이브러리 추가

---
+++ view/lib/popup.js
@@ -0,0 +1,13 @@
+﻿$(document).on("keydown", function(e) {
+	switch (e.keyCode) {
+		case 27: { // Esc
+			requestClose();
+			break;
+		}
+	}
+});
+
+// 종료 전 확인 필요한 경우 override
+function requestClose() {
+	window.close();
+}
