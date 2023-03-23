1b1909c7b60d71e5dd4b9eb3961b3389ce8e7844
Harne Nim <harne_@naver.com>
2023-03-23 22:54:50 +0900

    다른 데서 쓰던 군더더기 코드 정리

--- view/lib/webview.js
+++ view/lib/webview.js
@@ -1,10 +1,4 @@
-﻿var alt   = false;
-var ctrl  = false;
-var shift = false;
-var onMouseUp = null;
-var onMouseMove = null;
-
-var showDrag = false;
+﻿var showDrag = false;
 function setShowDrag(dragging) {
 	showDrag = dragging;
 }
@@ -21,21 +15,36 @@ function setDroppable() {
 	});
 }
 
-function call(names, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9) {
-	if (names.indexOf("refreshTime") < 0) {
-		console.log(names);
-	}
-	var obj = window;
-	names = names.split(".");
-	for (var i = 0; i < names.length - 1; i++) {
-		obj = obj[names[i]];
-	}
-	try {
-		var name = names[names.length - 1];
-		obj[name](p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
-	} catch (e) {
-	}
+// alert 재정의
+_alert = alert;
+_confirm = confirm;
+alert = function(msg) {
+	_alert(msg);
+	// TODO
+	//binder.alert(msg);
 }
+// confirm 재정의
+var afterConfirmYes = null;
+var afterConfirmNo  = null;
+confirm = function(msg, yes, no) {
+	var result = _confirm(msg);
+	if (result) {
+		if (yes) yes();
+	} else {
+		if (no) no();
+	}
+	
+	// TODO
+	/*
+	afterConfirmYes = yes;
+	afterConfirmNo  = no;
+	binder.confirm(msg);
+	*/
+	
+	return result;
+}
+
+var ctrl  = false;
 
 $(function () {
 	var doc = $(document);
@@ -46,17 +55,13 @@ $(function () {
 	});
 	window.onkeydown = function() {
 		switch(event.keyCode) {
-			case 16: shift = true; break;
-			case 17: ctrl  = true; break;
-			case 18: alt   = true; break;
+			case 17: ctrl = true; break;
 			case 116: return false; // F5 새로고침 방지
 		}
 	};
 	window.onkeyup = function() {
 		switch(event.keyCode) {
-			case 16: shift = false; break;
-			case 17: ctrl  = false; break;
-			case 18: alt   = false; break;
+			case 17: ctrl = false; break;
 		}
 	};
 	window.onmousewheel = function () {
@@ -65,17 +70,6 @@ $(function () {
 			return false;
 		}
 	};
-	window.addEventListener("mousemove", function() {
-		if (onMouseMove) {
-			onMouseMove(event);
-		}
-	});
-	window.addEventListener("mouseup", function() {
-		if (onMouseUp && typeof onMouseUp == "function") {
-			onMouseUp(event);
-			onMouseUp = true;
-		}
-	});
 
 	// 우클릭 방지
 	doc.on("contextmenu", function () {
@@ -84,27 +78,3 @@ $(function () {
 
 	if (window.binder) binder.initAfterLoad();
 });
-
-//alert 재정의
-_alert = alert;
-_confirm = confirm;
-alert = function(msg) {
-	_alert(msg);
-	// TODO
-	//binder.alert(msg);
-}
-confirm = function(msg, yes, no) {
-	var result = _confirm(msg);
-	if (result) {
-		if (yes) yes();
-	} else {
-		if (no) no();
-	}
-	
-	// TODO
-	afterConfirmYes = yes;
-	afterConfirmNo  = no;
-	//binder.confirm(msg);
-	
-	return result;
-}
